3cec7075b8a4ea6d00f1250e442db6523af24778
Hardus van der Berg <hardusvdberg@gmail.com>
2020-04-04 20:33:08 +0000

    new template - Web SideMenu

---
+++ view/templateWebSideMenu/dist/menu.js
@@ -0,0 +1,9 @@
+[
+  { "text": "my factory", "type": "divider" },
+  { "text": "Dahsboard", "type": "link", "glyph": "fas fa-tachometer-alt", "content": "content.html" },
+  { "text": "manage", "type": "divider" },
+  { "text": "Capabilities", "type": "list", "glyph": "fas fa-columns", "items": [
+    { "text": "active list", "content": "content2.html" },
+    { "text": "create new" }
+  ]}
+]
