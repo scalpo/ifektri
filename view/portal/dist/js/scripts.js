194488d65d8daedc400cf9a26ba0dd3442b2efbf
Hardus van der Berg <hardusvdberg@gmail.com>
2020-04-04 20:16:45 +0000

    new web template - side menu

--- view/portal/dist/js/scripts.js
+++ view/portal/dist/js/scripts.js
@@ -4,20 +4,20 @@
     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-sb-admin/blob/master/LICENSE)
     */
     (function($) {
-    "use strict";
+    'use strict';
 
     // Add active state to sidbar nav links
     var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+        $('#layoutSidenav_nav .sb-sidenav a.nav-link').each(function() {
             if (this.href === path) {
-                $(this).addClass("active");
+                $(this).addClass('active');
             }
         });
 
     // Toggle the side navigation
-    $("#sidebarToggle").on("click", function(e) {
+    $('#sidebarToggle').on('click', function(e) {
         e.preventDefault();
-        $("body").toggleClass("sb-sidenav-toggled");
+        $('body').toggleClass('sb-sidenav-toggled');
     });
 
     api('GET', '/admin/instructionType', null, function(a, b) {
@@ -26,6 +26,17 @@
 
     api('GET', 'https://ifektri-1--ifektriifu.repl.co/admin/instructionType', {}, function(a, b) {
       console.log(b);
+    });
+
+    $('a.nav-link').on('click', function(e) {
+      e.preventDefault();
+      if(!$(this).data('toggle')) {
+        console.log('replace content');
+
+        var $content = $('<div class="text-white">' + new Date() + '</div>');
+
+        $('#mainContent').append($content);
+      }
     });
 
 })(jQuery);
